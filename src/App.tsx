5b6d29f96144ab890585cb188c145dd5b9181d6f
Hacı Celal Aygar <celal.aygar@gmail.com>
2024-11-21 15:28:38 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import My0000 from './0000.svg';
 import MySVG from './component/MySVG';
+import MySVG2 from './component/MySVG2';
 
 function App() {
 
   return (
     <>
       <div style={{ padding: '20px' }}>
+        <MySVG2 />
+        <br/>
         <MySVG />
       </div>
     </>
