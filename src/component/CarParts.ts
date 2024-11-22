394225a24d6908b4bb38bceb30efd2cca5320f2d
Hacı Celal Aygar <celal.aygar@gmail.com>
2024-11-22 09:38:55 +0300

    Update CarParts.ts

--- src/component/CarParts.ts
+++ src/component/CarParts.ts
@@ -19,23 +19,49 @@ interface CarBodyPart {
     color: string;
     partChangeStatus: CarStatusType;
 }
+export const CarBodyPartsKeys = {
+    FRONT: "FRONT",
+    REAR: "REAR",
+    RIGHT_FRONT_DOOR: "RIGHT_FRONT_DOOR",
+    LEFT_FRONT_DOOR: "LEFT_FRONT_DOOR",
+    RIGHT_REAR_DOOR: "RIGHT_REAR_DOOR",
+    LEFT_REAR_DOOR: "LEFT_REAR_DOOR",
+    ROOF: "ROOF",
+    RIGHT_FRONT_FENDER: "RIGHT_FRONT_FENDER",
+    LEFT_FRONT_FENDER: "LEFT_FRONT_FENDER",
+    RIGHT_REAR_FENDER: "RIGHT_REAR_FENDER",
+    LEFT_REAR_FENDER: "LEFT_REAR_FENDER",
+    FRONT_BUMPER: "FRONT_BUMPER",
+    REAR_BUMPER: "REAR_BUMPER",
+} as const;
 
-interface CarBodyParts {
-    [key: string]: CarBodyPart;
+
+type CarBodyPartsKeysType = keyof typeof CarBodyPartsKeys;
+
+
+interface CarBodyPart {
+    color: string;
+    partChangeStatus: (typeof CarStatus)[keyof typeof CarStatus];
 }
 
-export let carBodyParts: CarBodyParts = {
-    "FRONT": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "REAR": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "RIGHT_FRONT_DOOR": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "LETF_FRONT_DOOR": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "RIGHT_REAR_DOOR": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "LETF_REAR_DOOR": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "ROOF": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "RIGHT_FRONT_FENDER": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "LEFT_FRONT_FENDER": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "RIGHT_REAR_FENDER": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "LEFT_REAR_FENDER": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "FRONT_BUMPER": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
-    "REAR_BUMPER": { color: CarColorStatus.DEFECT_FREE, partChangeStatus: CarStatus.DEFECT_FREE },
+
+type CarBodyParts {
+    [key in CarBodyPartsKeysType]: CarBodyPart;
+}
+
+// Örnek araba parçalarını oluştur
+export const carBodyParts: CarBodyParts = {
+    FRONT: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    REAR: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    RIGHT_FRONT_DOOR: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    LEFT_FRONT_DOOR: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    RIGHT_REAR_DOOR: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    LEFT_REAR_DOOR: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    ROOF: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    RIGHT_FRONT_FENDER: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    LEFT_FRONT_FENDER: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    RIGHT_REAR_FENDER: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    LEFT_REAR_FENDER: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    FRONT_BUMPER: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
+    REAR_BUMPER: { color: "#AABBAB", partChangeStatus: CarStatus.DEFECT_FREE },
 };
